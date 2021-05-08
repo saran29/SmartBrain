d7b3c06a662360d68860a14ae51147bf4ee8a643
Saran Jha <jhasaran9@gmail.com>
2021-05-07 18:49:09 -0700

    Updated frontend

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import 'tachyons';
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 
+const particleOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 class App extends Component {
-  render(){
+  render() {
     return (
       <div className="App">
+        <Particles className='particles'
+          params={particleOptions}
+        />
         <Navigation />
-        {/* <Logo />
+        <Logo />
+        <Rank />
         <ImageLinkForm />
-        <FaceRecognition /> */}
+        {/*<FaceRecognition /> */}
       </div>
     );
   }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div className='white f3'>
+            {'Saran your current rank is...'}
+            <div className='white f1'>
+                {'#1'}
+            </div>
+           
+        </div>
+    );
+}
+
+export default Rank;
