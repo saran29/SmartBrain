e5491782848d45b84771b1cd1cd1fd6631775b93
Saran Jha <jhasaran9@gmail.com>
2021-05-31 16:32:24 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -38,6 +38,6 @@ app.post('/imageUrl', (req,res) => { image.handleApiCall(req,res) })
 app.get('/profile/:id', (req, res) => { profile.handleProfile(db)(req, res) })
 
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
